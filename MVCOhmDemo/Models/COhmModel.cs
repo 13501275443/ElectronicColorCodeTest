e58dcd1731c3652f55f1f7b22c2de7a560dd9e58
13501275443 <robertbaggio51567@hotmail.com>
2017-11-17 19:14:46 -0600

    Add the color change effect once user select band color from dropdown list

--- MVCOhmDemo/Models/COhmModel.cs
+++ MVCOhmDemo/Models/COhmModel.cs
@@ -21,14 +21,8 @@ namespace MVCOhmDemo.Models
             m_OhmValueCalculator = new COhmValueCalculator();
             SetupColorBand(firstSignificant, secondSignificant, multiplierColor, toleranceColor);
         }
-        //public void CalculateOhmValue(string firstSignificant, string secondSignificant, string multiplierColor, string toleranceColor)
-        //{
-        //    m_OhmValue = m_OhmValueCalculator.CalculateOhmValue(firstSignificant, secondSignificant, multiplierColor, toleranceColor, out m_ToleranceRange);
-        //}
-        public string GetOhmValue()
-        {
-            return m_OhmValue.ToString();
-        }
+        
+        
 
         [DisplayName("FirstSignificantFigureList")]
         public SelectList FirstSignificantFigureList
@@ -81,14 +75,77 @@ namespace MVCOhmDemo.Models
                 list.Add(key);
             }
             ToleranceList = new SelectList(list, toleranceColor);
+            m_BandAColor = firstSignificant;
+            m_BandBColor = secondSignificant;
+            m_BandCColor = multiplierColor;
+            m_BandDColor = toleranceColor;
+            if (m_BandDColor.Length == 0)
+                m_BandDColor = "none";
+            float fOhmValue = m_OhmValueCalculator.CalculateOhmValue(firstSignificant, secondSignificant, multiplierColor, toleranceColor, out m_ToleranceRange);
 
-            m_OhmValue = m_OhmValueCalculator.CalculateOhmValue(firstSignificant, secondSignificant, multiplierColor, toleranceColor, out m_ToleranceRange);
+            m_OhmValueString = ConvertFloatToString(fOhmValue);
             return ;
 
         }
 
+        private string ConvertFloatToString(float fOhmValue)
+        {
+            if (fOhmValue / 1000 < 1.0)
+                return fOhmValue.ToString();
+            else if(fOhmValue / 1000 > 1.0f && fOhmValue / 1000 < 1000.0f)
+            {
+                fOhmValue = fOhmValue / 1000;
+                return fOhmValue.ToString()+"K";
+            }
+            else if(fOhmValue / 1000000 >1.0f && fOhmValue /1000000<1000.0f)
+            {
+                fOhmValue = fOhmValue / 1000000;
+                return fOhmValue.ToString()+"M";
+            }
+            else
+            {
+                fOhmValue = fOhmValue / 1000000000;
+                return fOhmValue.ToString() + "B";
+            }
+
+        }
+        public string OhmValue
+        {
+            get { return m_OhmValueString; }
+        }
+
+        public float ToleranceRange
+        {
+            get { return m_ToleranceRange; }
+
+        }
+        public string BandAColor
+        {
+            get { return m_BandAColor; }
+        }
+
+        public string BandBColor
+        {
+            get { return m_BandBColor; }
+        }
+
+        public string BandCColor
+        {
+            get { return m_BandCColor; }
+        }
+
+        public string BandDColor
+        {
+            get { return m_BandDColor; }            
+        } 
         private COhmValueCalculator m_OhmValueCalculator;
-        private float m_OhmValue;
+        private string m_OhmValueString;
         private float m_ToleranceRange;
+        private string m_BandAColor;
+        private string m_BandBColor;
+        private string m_BandCColor;
+        private string m_BandDColor;
+
+               
     }
 }
